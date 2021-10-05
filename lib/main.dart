4885a08130720695e97f9ad138cd216c41031c4e
Parthrathod6103 <parthrathod6103@gmail.com>
2021-10-05 14:01:18 +0530

    done

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:firebase/Login.dart';
+import 'package:firebase/SignUp.dart';
+import 'package:firebase/Start.dart';
+import 'package:flutter/material.dart';
+import 'HomePage.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+   runApp(MyApp());
+   }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      theme: ThemeData(
+        primaryColor: Colors.orange
+      ),
+      debugShowCheckedModeBanner: false,
+      home: 
+    
+      Start(),
+
+      routes: <String,WidgetBuilder>{
+
+        "Login" : (BuildContext context)=>Login(),
+        "SignUp":(BuildContext context)=>SignUp(),
+        "start":(BuildContext context)=>Start(),
+      },
+      
+    );
+  }
+
+}
+
+
+
